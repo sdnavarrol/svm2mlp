bbf96f330f92bfd3795f79aee35073f76ce4c3db
Sergio Lopez <sdnavarrol@gmail.com>
2018-02-12 15:47:20 -0300

    Normalization. To define threshold value for optimal binarization

--- code/resize.cpp
+++ code/resize.cpp
@@ -33,19 +33,17 @@ int main( int argc, char** argv )
   	compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
     compression_params.push_back(9);
 	
-	for (i=0;i<5;i++){
+	for (i=0;i<25;i++){
 		string fileName = "../images/a/" + to_string(i) + ".png";
 		src = imread(fileName,IMREAD_COLOR);
 		if(src.empty()){return -1;}
-		string newFileName = "output/gray/" + to_string(i) + "_gray.png";
+		string newFileName = "../images/a/" + to_string(i) + "_gray.png";
         cvtColor( src, src_gray, COLOR_BGR2GRAY ); // Convert the image to Gray
        	imwrite(newFileName, src_gray, compression_params);
-       	for(j=0;j<255;j++){
-       		newFileName = "output/threshold/" + to_string(i) + "_threshold_iter_" + to_string(j) +".png";
-       		threshold( src_gray, src_thr, j, max_BINARY_value,threshold_type );
-	   		imwrite(newFileName, src_thr, compression_params);
-       	}
-   		newFileName = "output/resize/" + to_string(i) + "_sized.png";
+  		newFileName = "../images/a/" + to_string(i) + "_threshold.png";
+     	threshold( src_gray, src_thr, 124, max_BINARY_value,0);
+		imwrite(newFileName, src_thr, compression_params);
+       	newFileName = "../images/a/" + to_string(i) + "_sized.png";
 		resize(src_thr,dst,dst.size(),70,70,0);
     	imwrite(newFileName, dst, compression_params);
     }
